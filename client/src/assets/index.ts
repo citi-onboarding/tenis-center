7fab2ff26bc630d52fc9935b3ad2795a573aa168
Pedro Fernandes Barreto Costa <fernandespedrobc@gmail.com>
2022-06-07 16:35:38 -0300

    Partners Section

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -1,5 +1,8 @@
 import LogoCITi from './logo.png';
+import LogoPB from './Logo B&W.png';
+import Next from './NextArrow.png';
+import Prev from './PrevArrow.png';
 
 export {
-    LogoCITi
+    LogoCITi, LogoPB, Next, Prev
 };
---
+++ client/src/pages/Partners/index.tsx
@@ -0,0 +1,83 @@
+import React, {useEffect,useState} from 'react';
+import { LogoPB, Next, Prev } from '../../assets';
+import { Card } from '../../components';
+import { PartnersBackgroud, Title, Blue, Pai, Filho, CardPartner } from "./styles"
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import axios from "axios";
+
+type CardProps = {
+    image2: string;
+};
+
+const PrevArrow: React.ElementType = (props) => {
+  const { className, onClick, style } = props;
+  return (
+      <div
+        className={className}
+        style={{ ...style, top: "-28px", zIndex:"1", left: "-15px"}}
+        onClick={onClick}
+      >
+        <img src={Prev} alt="" />
+      </div>
+  );
+}
+
+const NextArrow: React.ElementType = (props) => {
+  const { className, onClick, style } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, top: "-28px", right: "78px" }}
+      onClick={onClick}
+    >
+      <img src={Next} alt="" />
+    </div>
+  );
+}
+
+
+const Slide: React.ElementType = () => {
+  const [CarrosselInfo, setCarrosselInfo] = useState<CardProps[]>([]);
+
+  async function loadCarrosselInfo() {
+      const res = await axios.get('/partners');
+      const { data } = res;
+      console.log(data);
+      setCarrosselInfo(data);
+  }
+
+  useEffect(() => {
+    loadCarrosselInfo();
+  }, [])
+
+  var settings = {
+      infinite: true,
+      speed: 500,
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      prevArrow: <PrevArrow/>,
+      nextArrow: <NextArrow/>,
+    };
+    return (
+      <Pai>
+        <Filho>
+          <Slider {...settings}>
+            {CarrosselInfo?.map(({image2}) => (
+                  <Card image={image2}/>
+              ))}
+          </Slider>
+        </Filho>
+      </Pai>
+    );
+  }
+
+export const Partners: React.ElementType = ({image2}: CardProps) => {
+    return (
+        <PartnersBackgroud>
+            <Title>Nossos <Blue>parceiros</Blue></Title>
+            <Slide/>
+        </PartnersBackgroud>
+    );
+}
