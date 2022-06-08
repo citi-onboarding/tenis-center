66498a807ee0f1cd104b98987e176095475ef11f
Pedro Fernandes Barreto Costa <fernandespedrobc@gmail.com>
2022-06-08 15:02:11 -0300

    Removed Background_Header

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -4,8 +4,7 @@ import prevArrow from './prev.png';
 import LogoPB from './Logo B&W.png';
 import Next from './BiggerNextArrow.png';
 import Prev from './PrevArrow.png';
-import Background_Header from './Background-Header.png';
 
 export {
-    LogoCITi, nextArrow, prevArrow, LogoPB, Next, Prev, Background_Header
+    LogoCITi, nextArrow, prevArrow, LogoPB, Next, Prev
 };
