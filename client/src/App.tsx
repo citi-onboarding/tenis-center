9115f7522f904c74fede8e5026d07016eda83824
thaisnevest <neves.thais@hotmail.com>
2022-06-08 14:18:57 -0300

    alterations done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,15 +2,14 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
 import { GlobalStyle } from './styles/global';
-import { Home, Partners, Service} from './pages';
+import { Partners, Service } from './pages';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Home/>
+      <Service/>
       <Partners/>
       <GlobalStyle/>
-      <Service/>
     </ThemeProvider>
 
   );
