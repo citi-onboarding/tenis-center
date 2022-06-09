fc1fd140ba59b7ef5cc7b786bebbc0134a545981
Pedro Fernandes Barreto Costa <fernandespedrobc@gmail.com>
2022-06-09 12:01:35 -0300

    Added arrow animation

--- client/src/pages/Header/index.tsx
+++ client/src/pages/Header/index.tsx
@@ -1,12 +1,18 @@
 import React from "react"
-import { ParentDiv,  BackgroundImage, MainTitle, HeaderDescription, TextHolder, DownArrow, NullDiv } from "./styles";
+import { ParentDiv,  BackgroundImage, MainTitle, HeaderDescription, TextHolder, DownArrow, NullDiv, ArrowLeft, ArrowRight, Anchor, ArrowHolder } from "./styles";
 import { Background_Header, ArrowHeader } from '../../assets';
 
 type HeaderTennisProps = {
 
 };
 
+
 export const HeaderTennis: React.ElementType = ({}: HeaderTennisProps) => {
+    
+    function ChangeColor(){
+        console.log("Salve.")
+    }
+    
     return (
         <ParentDiv>
             <BackgroundImage src={Background_Header}/>
@@ -15,7 +21,7 @@ export const HeaderTennis: React.ElementType = ({}: HeaderTennisProps) => {
                 <HeaderDescription>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget lacus fermentum adipiscing elementum in placerat orci tellus vel. Integer quam mattis aliquam urna, in amet.</HeaderDescription>
                 <HeaderDescription>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget lacus fermentum adipiscing elementum in placerat orci tellus vel.</HeaderDescription>
             </TextHolder>
-                <a href="#service"><DownArrow src={ArrowHeader}/></a>
+                <Anchor href="#service"><ArrowHolder onClick={ChangeColor}><ArrowLeft/><ArrowRight/></ArrowHolder></Anchor>
                 <NullDiv id="service"></NullDiv>  
         </ParentDiv>
     );
--- client/src/pages/Header/styles.ts
+++ client/src/pages/Header/styles.ts
@@ -30,7 +30,7 @@ export const HeaderDescription = styled.p`
     position: relative;
     top: 16px;
     width: 590px;
-    height: 100px;
+    height: 95px;
 
     font-family: 'Barlow';
     font-style: normal;
@@ -49,11 +49,76 @@ export const TextHolder = styled.div`
 
 export const DownArrow = styled.img`
     position: absolute;
-    left: 695px;
-    top: 675px;
+    left: 685px;
+    top: 660px;
+    border: 2px solid white;
+    border-radius: 50%;
+    padding: 1px 5px 11px 5px;
+`
+
+export const ArrowRight = styled.div`
+
+    height: 3.898548640916105px;
+    width: 26.355830988298777px;
+    
+    position: absolute;
+    left: 20px;
+    bottom: 20px;
+
+    background: #F5F5F5;
+    transform: matrix(-0.74, 0.64, 0.7, 0.74, 0, 0);
+`
+
+export const ArrowLeft = styled.div`
+    height: 3.898548640916105px;
+    width: 26.355830988298777px;
+
+    position: absolute;
+    left: 4px;
+    bottom: 20px;
+
+    background: #F5F5F5;
+    transform: matrix(0.74, 0.64, -0.7, 0.74, 0, 0);
 `
 
 export const NullDiv = styled.div`
     width: 0px;
     height: 0px;
+`
+
+export const Anchor = styled.a`
+    width: 50px;
+    height: 3.898548640916105px;
+    border: 2px solid white;
+    border-radius: 50%;
+    padding: 1px 5px 11px 5px;
+`
+
+export const ArrowHolder = styled.div`
+    position: absolute;
+    right: 700px;
+    bottom: 42px;
+
+    width: 55px;
+    height: 55px;
+    border: 2.3px solid rgba(255,255,255,0);
+    border-radius: 50%;
+    margin: 0px;
+    
+    &:hover{
+        border: 2.3px solid rgba(245,245,245,1);
+    }
+
+    &:active{
+        border: 2.3px solid rgba(245,245,245,1); 
+        background: #F5F5F5;
+
+        ${ArrowLeft}{
+            background: black;
+        }
+
+        ${ArrowRight}{
+            background: black;
+        }
+    }
 `
