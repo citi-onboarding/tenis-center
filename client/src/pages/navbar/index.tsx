4555802405694bdbd74cc7e248fc645f045e0fca
emilly <emillycristine06@gmail.com>
2022-06-09 17:57:12 -0300

    put height and width to Fale Conosco

--- client/src/pages/navbar/index.tsx
+++ client/src/pages/navbar/index.tsx
@@ -6,11 +6,11 @@ export const Navbar: React.ElementType=  () =>  {
     return(
      <NavbarContainer>
          <NavbarImage src = { logonavbar }/>
-         <ButtonContainer href=""> <ButtonComp text= "Início" backgroundColor= "#0476D9" /></ButtonContainer >
+         <ButtonContainer href=""> <ButtonComp text= "Início" /></ButtonContainer >
          <ButtonContainer href=""> <ButtonComp text= "Projetos" fontWeight={500}/></ButtonContainer>
          <ButtonContainer href=""> <ButtonComp text= "Serviços" fontWeight={500}/></ButtonContainer>
          <ButtonContainer href=""> <ButtonComp text= "Parceiros" fontWeight={500}/></ButtonContainer>
-         <ButtonContainer href=""> <ButtonComp text= "Fale Conosco" fontWeight={500}/></ButtonContainer >
+         <ButtonContainer href=""> <ButtonComp text= "Fale Conosco" backgroundColor= "#0476D9" height={24} width= {115} fontWeight={500}/></ButtonContainer >
     </NavbarContainer>
  
     ) 
