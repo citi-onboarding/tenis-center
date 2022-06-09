8bf9095c92781312ec4c4371b1d127f964e7f511
emilly <emillycristine06@gmail.com>
2022-06-09 18:14:18 -0300

    put LastButton as a div

--- client/src/pages/navbar/index.tsx
+++ client/src/pages/navbar/index.tsx
@@ -1,16 +1,16 @@
 import ButtonComp from "../../components/buttoncomponent";
-import { NavbarContainer, NavbarImage, ButtonContainer} from './style';
+import { NavbarContainer, NavbarImage, ButtonContainer, LastButton} from './style';
 import { logonavbar } from '../../assets/index';
 
 export const Navbar: React.ElementType=  () =>  {
     return(
      <NavbarContainer>
          <NavbarImage src = { logonavbar }/>
-         <ButtonContainer href=""> <ButtonComp text= "Início" /></ButtonContainer >
+         <ButtonContainer href=""> <ButtonComp text= "Início" fontWeight={500}/></ButtonContainer >
          <ButtonContainer href=""> <ButtonComp text= "Projetos" fontWeight={500}/></ButtonContainer>
          <ButtonContainer href=""> <ButtonComp text= "Serviços" fontWeight={500}/></ButtonContainer>
          <ButtonContainer href=""> <ButtonComp text= "Parceiros" fontWeight={500}/></ButtonContainer>
-         <ButtonContainer href=""> <ButtonComp text= "Fale Conosco" backgroundColor= "#0476D9" height={44} width= {171} fontWeight={500}/></ButtonContainer >
+         <LastButton href=""> <ButtonComp text= "Fale Conosco" backgroundColor= "#0476D9" height={44} width= {171} fontWeight={500}/></LastButton >
     </NavbarContainer>
  
     ) 
